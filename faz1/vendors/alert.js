2ac41f45399a89ca34de82f25743343736fdedc8
Serkan Kocaman <kocaman.serkan98@gmail.com>
2021-06-22 12:02:11 +0300

    cleaning

--- faz1/vendors/alert.js
+++ faz1/vendors/alert.js
@@ -2,44 +2,45 @@
 
 /* Starts from here */
 
-function alertMsg(msg, type) {
+//function alertMsg(msg, type) {
 
-    if (type == 'yes') {
-        $.toast({
-            heading: 'İşlem Başarılı',
-            text: msg,
-            icon: 'success',
-            loader: true,
-            loaderBg: '#fff',
-            showHideTransition: 'fade',
-            hideAfter: 3000,
-            allowToastClose: false,
-            position: {
-                right: 100,
-                left: 0,
-                top: 50
-            },
-
-
-        })
-    } else {
-        $.toast({
-            heading: 'Error',
-            text: msg,
-            icon: 'error',
-            loader: true,
-            loaderBg: '#fff',
-            showHideTransition: 'plain',
-            hideAfter: 3000,
-            position: {
-                left: 100,
-                top: 30
-            }
-        })
-    }
+//    if (type == 'yes') {
+//        $.toast({
+//            heading: 'İşlem Başarılı',
+//            text: msg,
+//            icon: 'success',
+//            loader: true,
+//            loaderBg: '#fff',
+//            showHideTransition: 'fade',
+//            hideAfter: 3000,
+//            allowToastClose: false,
+//            position: {
+//                right: 100,
+//                left: 0,
+//                top: 50
+//            },
 
 
-}
+//        })
+//    } else {
+//        $.toast({
+//            heading: 'Hata',
+//            text: msg,
+//            icon: 'error',
+//            loader: true,
+//            loaderBg: '#fff',
+//            showHideTransition: 'plain',
+//            hideAfter: 3000,
+//            position: {
+//                right: 100,
+//                left: 0,
+//                top: 50
+//            }
+//        })
+//    }
+
+
+//}
 
 
 //$("#error").click(function () {
---
+++ faz1/vendors/alertFunc.js
@@ -0,0 +1,37 @@
+﻿function alertMsg(msg, type) {
+
+    if (type == 'yes') {
+        $.toast({
+            heading: 'İşlem Başarılı',
+            text: msg,
+            icon: 'success',
+            loader: true,
+            loaderBg: '#fff',
+            showHideTransition: 'fade',
+            hideAfter: 3000,
+            allowToastClose: false,
+            position: {
+                right: 100,
+                left: 0,
+                top: 50
+            },
+
+
+        })
+    } else {
+        $.toast({
+            heading: 'Hata',
+            text: msg,
+            icon: 'error',
+            loader: true,
+            loaderBg: '#fff',
+            showHideTransition: 'plain',
+            hideAfter: 3000,
+            position: {
+                right: 100,
+                left: 0,
+                top: 50
+            }
+        })
+    }
+}
